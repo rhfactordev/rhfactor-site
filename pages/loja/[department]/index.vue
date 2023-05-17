0830c8b0ef0eeb0bb60209714b45512037d3f092
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-05-17 08:57:22 -0300

    Navegação da Loja

---
+++ pages/loja/[department]/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const route = useRoute()
+const departmentSource = computed(()=> route.params.department )
+</script>
+
+<template>
+  <main class="grid grid-cols-4 gap-2">
+    <h1>Index do {{departmentSource}}</h1>
+  </main>
+</template>
---
+++ pages/loja/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Loja online</h1>
+</template>

d8e60fffc7b271772d96bad8a6dd35dd633bdac4
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-05-19 17:32:49 -0300

    Departamento

--- pages/loja/[department]/[source].vue
+++ pages/loja/[department]/[source].vue
@@ -1,7 +1,29 @@
 <script setup>
+import {useNuxtApp} from "#app";
+
 const route = useRoute()
 const pageSource = computed(()=> route.params.source )
 const departmentSource = computed(()=> route.params.department )
+
+const department = computed(()=>{
+  return {
+    name : "Loja Virtual",
+    description: "Descrição da loja",
+    image: "image"
+  }
+})
+
+const site = useNuxtApp().site
+const meta = {
+  title: `${department.value.name} - ${site.title}`,
+  ogTitle: `${department.value.name} - ${site.title}`,
+  description: `${department.value.description}`,
+  ogDescription: `${department.value.description}`,
+  ogImage: `${department.value.image}`
+}
+
+useSeoMeta(meta)
+useServerSeoMeta(meta)
 </script>
 
 <template>
--- pages/loja/index.vue
+++ pages/loja/index.vue
@@ -1,19 +1,7 @@
-
 <script setup>
 import ETitle from "~/components/elements/ETitle.vue";
 import {useNuxtApp} from "#app";
 
-const site = useNuxtApp().site
-const meta = {
-  title: `Loja Virtual - ${site.title}`,
-  ogTitle: `Loja Virtual - ${site.title}`,
-  description: `Conheça e adquira os produtos de ${site.title}`,
-  ogDescription: `Conheça e adquira os produtos de ${site.title}`,
-  ogImage: site.image
-}
-
-useSeoMeta(meta)
-useServerSeoMeta(meta)
 
 </script>
 

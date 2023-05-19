2f5c4299d97ef3ef0fd056e1e83bc692c75e1289
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-05-19 20:11:34 -0300

    Fix navegação de paginas

--- pages/loja/[department]/page/[page].vue
+++ pages/loja/[department]/page/[page].vue
@@ -6,7 +6,7 @@ import {useNuxtApp} from "#app";
 
 const route = useRoute()
 const departmentSource = computed(()=> route.params.department )
-const page = computed(()=> route.params.page >= 1 ? route.params.page : 1 )
+const page = computed(()=> route.params.page ? route.params.page : 1 )
 
 const hasDepartment = computed(()=> departmentSource.value != null)
 
@@ -25,14 +25,14 @@ const departments = [
   {
     name: 'Departamento 1',
     source: 'departamento-1',
-    target: '/loja/departamento-1/',
+    target: '/loja/departamento-1',
     description: 'Descrição do departamento 1',
     image : 'image'
   },
   {
     name: 'Departamento 2',
     source: 'departamento-2',
-    target: '/loja/departamento-2/',
+    target: '/loja/departamento-2',
     description: 'Descrição do departamento 1',
     image : 'image'
   }
@@ -57,7 +57,8 @@ useServerSeoMeta(meta)
     <e-title class="col-span-4">{{department.name}}</e-title>
     <div class="md:order-2 col-span-4 md:col-span-3 p-4">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        Itens
+        Você está na {{department.name}}
+        E está na página {{page}}
       </div>
       <div v-if="hasDepartment" class="text-center my-3">
         <nav-pagination :path="department.target"

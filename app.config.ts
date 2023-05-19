f677f190f76fb6e7469a69addd0aeee7e0c8dcd4
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-05-19 20:11:42 -0300

    Ajustar conteudo estatico

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,14 @@ export default defineAppConfig({
         {charset: "utf-8"}
     ],
     routeRules: {
+        '/cadastro': {static: true},
+        '/contato': {static: true},
+        '/esqueci-minha-senha': {static: true},
+        '/login': {static: true},
+        '/loja/**': {static: true},
         '/privacidade': {static: true},
+        '/sobre-mim': {static: true},
         '/termos-de-uso': {static: true},
+
     }
 })

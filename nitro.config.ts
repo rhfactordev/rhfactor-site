4048724626f278109135aafb26995f38810a8986
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-05-25 09:18:44 -0300

    Revert "Adicionar páginas ao prerender"
    This reverts commit 98e797c8a57f36383cb8e1a17353e47ff09112c1.

--- nitro.config.ts
+++ nitro.config.ts
@@ -6,25 +6,12 @@ export default defineNitroConfig({
         '/contato': {static: true},
         '/esqueci-minha-senha': {static: true},
         '/login': {static: true},
-        '/blog/**': {static: true},
         // '/loja/**': {static: true},
         '/privacidade': {static: true},
         '/sobre-mim': {static: true},
         '/termos-de-uso': {static: true},
-        '/minha-conta/**' : { index: false }
     },
     prerender: {
-        routes:[
-            '/cadastro',
-            '/contato',
-            '/esqueci-minha-senha',
-            '/login',
-            '/blog',
-            '/blog/**',
-            '/privacidade',
-            '/sobre-mim',
-            '/termos-de-uso',
-        ],
         ignore: ['/minha-conta']
     },
 });

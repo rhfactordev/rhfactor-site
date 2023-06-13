3cb11a364195715237dc6a9062dcb00528f4c705
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-06-13 13:03:17 -0300

    Verificar data de login

--- stores/session.ts
+++ stores/session.ts
@@ -4,21 +4,33 @@ import {useLocalStorage} from '@vueuse/core';
 const initialUser : User = { name : null , id : null }
 const initialUserToken : UserToken = { accessToken : null, expiresIn : null }
 
+const hasntExpired = (lastLogin, expiresIn) =>{
+    lastLogin.setMinutes( null, expiresIn )
+    return lastLogin > new Date()
+}
+
 export const useSessionStore = defineStore('session', {
     state: () => ({
         userToken : useLocalStorage('userToken', initialUserToken) ,
-        user : useLocalStorage('user', initialUser)
+        user : useLocalStorage('user', initialUser),
+        lastLoginDate: null,
     }),
     actions: {
         login(userToken: UserToken) {
             this.userToken = userToken
+            this.lastLoginDate = new Date()
         },
         logout(){
             this.userToken = initialUserToken
+            this.lastLoginDate = null
         }
     },
     getters: {
-        isAuthenticated: state => state.userToken != null && state.userToken.accessToken != null,
+        isAuthenticated: state => state.userToken != null
+            && state.userToken.accessToken != null
+            && state.userToken.expiresIn != null
+            && state.lastLoginDate != null
+            && hasntExpired(state.lastLoginDate, state.userToken.expiresIn),
         userDisplayName : state => state.userToken.expiresIn,
         token: state => state.userToken.accessToken
     },

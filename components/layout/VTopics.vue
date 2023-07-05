8871ebd64870fb48d4a86454e73c1db35859c8e1
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-07-05 17:04:38 -0300

    Update VTopics.vue

--- components/layout/VTopics.vue
+++ components/layout/VTopics.vue
@@ -1,11 +1,21 @@
 <script setup>
-defineProps(['topics'])
+const props = defineProps(['topics'])
+
+const jsonItem = computed(()=> {
+  try {
+    return JSON.parse(props.topics)
+  }catch (e){
+    console.log('jsonItem', e)
+    return []
+  }
+})
+
 </script>
 <template>
   <div class="md:-mt-24 lg:-mt-48">
     <div class="container mx-auto">
       <div class="flex flex-wrap md:flex-nowrap">
-        <div v-for="({pre, title, description}, i) in topics"
+        <div v-for="({pre, title, description}, i) in jsonItem"
              :key="i"
              class="bg-cyan-200 hover:bg-cyan-500 transition-colors w-full p-10 mx-3 mb-3">
           <p class="text-cyan-800 font-bold text-3xl">{{pre}}</p>

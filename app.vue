a9c370d1f09f2a1286c5087541c99c8de64b7f2d
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-07-11 18:23:52 -0300

    Página de Links

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <script setup>
 import {useNuxtApp} from "#app";
+
 const app = useNuxtApp()
+const route = useRoute()
+
 const layout = computed(()=>{
+  if(route.fullPath === '/links'){
+    return false
+  }
   return app.site.layout
 })
 </script>
--- pages/links.vue
+++ pages/links.vue
@@ -1,56 +1,47 @@
 <script setup>
 import {useNuxtApp} from "#app";
-
-const { data } = await useFetch('/api/links')
-const links = data.value.links
+import VSocial from "~/components/layout/VSocial.vue";
 
 const site = useNuxtApp().site
-const meta = {
-  title: `Conecte-se`,
-  ogTitle: `Conecte-se`,
+
+const links = computed(() => site.links)
+const social = computed(() => site.social)
+
+const metatags = computed(() => ({
+  title: `Links`,
+  ogTitle: `Links`,
   description: site.description,
   ogDescription: site.description,
   ogImage: site.image,
-}
+}))
 
-definePageMeta({
-  layout: false
-});
-
-useSeoMeta(meta)
-useServerSeoMeta(meta)
+useSeoMeta(metatags.value)
+useServerSeoMeta(metatags.value)
 </script>
 <template>
-  <main class="bg-teal-600 px-5 py-10">
-    <div class="w-full md:w-5/12 lg:4/12 m-auto text-center text-white">
-
-      <img :src="data.image"
-           alt="Andreza Ferreira Astróloga"
-           class="border-2 border-neutral-100 rounded-full w-40 my-5 mx-auto h-40 object-cover">
-      <h1 class="font-extralight font-sans text-md mb-4">{{ data.name }}</h1>
-
-      <p class="font-normal text-md mb-4">{{ data.intro }}</p>
-
-      <a v-for="({name,target},x) in links" :class="{ 'dark' : x==0 }" :key="x" class="linkButton" :title="name" :href="target" target="_blank">{{name}}</a>
-
-      <img class="w-100 mb-4" :src="site.image" :alt="`Logo de ${site.title}`">
-
-      <layout-social :social="site.social" />
+  <header class="bg-neutral-200">
+    <div class="container text-center mx-auto">
+      <img class="w-96 mx-auto mb-3 " :src="site.image" :alt="`Logo ${site.title}`">
     </div>
+  </header>
+  <main>
+    <section class="container mx-auto">
+      <img class="w-48 mx-auto mb-3 rounded-full " :src="site.profileImage" :alt="`Profile image for ${site.title}`">
+      <div class="mx-auto w-9/12 text-sm">
+        <p>{{site.description}}</p>
+      </div>
+      <ul class="text-center mx-auto w-full sm:w-10/12 p-5">
+        <li class="rounded-lg shadow bg-neutral-200 transition-colors hover:bg-neutral-400 border border-neutral-300 mb-3 py-3" v-for="({name, target, icon}, i) in links" :key="i">
+          <IconCSS class="mr-3" v-if="icon.length > 2" :name="icon" />
+          <nuxt-link :to="target">{{name}}</nuxt-link>
+        </li>
+      </ul>
+    </section>
   </main>
+  <footer class="bg-neutral-900">
+    <v-social class="py-5" :social="social" />
+    <div class="bg-neutral-900 pb-10">
+      <img class="w-96 m-auto" :src="site.image" :alt="`Logo ${site.title}`">
+    </div>
+  </footer>
 </template>
-
-<style scoped type="postcss">
-.linkButton {
-  @apply bg-teal-200 p-2 text-neutral-700 w-full block rounded-md mb-3 shadow shadow-inner;
-&:hover {
-   @apply bg-teal-500 text-white;
- }
-&:disabled{
-   @apply bg-teal-900 text-gray-400;
- }
- &.dark{
-    @apply bg-teal-800 text-white;
-  }
-}
-</style>

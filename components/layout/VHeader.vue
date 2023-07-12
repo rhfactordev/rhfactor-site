533ae9ce9ac577afe0f760b44a0c6a6786346355
Roberto Fonseca Alves <roberto.alves@rhfactor.com.br>
2023-07-12 00:16:08 -0300

    Update VHeader.vue

--- components/layout/VHeader.vue
+++ components/layout/VHeader.vue
@@ -1,5 +1,18 @@
-<template>
-  <div>
+<script setup>
+import {useNuxtApp} from "#app";
 
-  </div>
+const site = useNuxtApp().site
+
+</script>
+
+<template>
+  <header>
+    <div class="container mx-auto px-10">
+      <div class="flex flex-wrap justify-between">
+        <nuxt-link class="max-w-[245px] sm:max-w-full" to="/">
+          <img class="w-full max-w-sm" :src="site.image" :alt="`Logo de ${site.title}`">
+        </nuxt-link>
+      </div>
+    </div>
+  </header>
 </template>
